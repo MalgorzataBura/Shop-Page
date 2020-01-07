98c0c7135d16059723090c56cda13a28124b237a
mbura <m.bura@interia.pl>
2020-01-07 19:29:19 +0100

    added form-input and custom-button

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from "./components/shop/shop.component";
+import Header from "../src/components/header/header.component";
+import SignInAndSignUpPage from "../src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
